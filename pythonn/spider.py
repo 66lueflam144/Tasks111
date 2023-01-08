358d75fe168c6d08391f2968a5df5ec4feb3d67d
66lueflam144 <117815219+66lueflam144@users.noreply.github.com>
2023-01-08 23:54:50 +0800

    Create spider.py

---
+++ pythonn/spider.py
@@ -0,0 +1,25 @@
+import re
+import os
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from PIL import Image
+from io import BytesIO
+
+img1='https://itawenya.cn/img/time.png'
+url1="https://itawenya.cn/"
+
+rep=requests.get(url1)
+html=rep.text
+print(html)
+
+dir_name=re.findall('<p style="(.*?)">',html)
+urls=re.findall('<img src="(.*?)" alt=".*?">',html)
+print(dir_name)
+print(urls)
+for url in urls:
+    file_name=url.split('/')[-1]
+    rep=requests.get(url1)
+    with open(file_name,'wb') as f:
+        f.write(rep.content)
